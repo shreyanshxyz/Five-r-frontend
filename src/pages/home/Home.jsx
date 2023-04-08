a1ba3dde179b9b15883332d15c91974fc072c80b
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-09 00:41:42 +0530

    trusted by component completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 
 const Home = () => {
   return (
     <div>
       <Featured />
+      <TrustedBy />
     </div>
   );
 };
