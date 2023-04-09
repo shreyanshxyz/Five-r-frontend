dd59becc2088d4f3c10773a28e625c0a24611e17
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-09 22:00:27 +0530

    Categories Card Documented

--- src/components/catCard/CatCard.jsx
+++ src/components/catCard/CatCard.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import "./CatCard.scss";
 
 function CatCard({ card }) {
+  // When data is fetched from a database, we use the card prop which then goes to the database and pulls the card's image, description, title.
+
   return (
     <Link to="/gigs?cat=design">
       <div className="catCard">
