57c3f67386b3285cbed9560d93b01191be404687
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-09 21:55:39 +0530

    Slider Documented

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Slide.scss";
 import Slider from "infinite-react-carousel";
 
+// The three props are the ones which will be passed down whenever we use the slider from the carousel so that the three props control that specific slideshow.
+
 const Slide = ({ children, slidesToShow, arrowsScroll }) => {
   return (
     <div className="slide">
