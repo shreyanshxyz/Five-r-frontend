3f7ea2ad15230ce22118b22ee84e7223a184b7dc
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-09 19:36:52 +0530

    Gig Cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
   return (
     <div className="home">
       <Featured />
-
       <TrustedBy />
       <Slide slidesToShow={5} arrowsScroll={5}>
         {cards.map((card) => (
