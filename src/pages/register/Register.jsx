9c8d9963399565ccec4df5101b8a54dea463c535
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-15 16:56:02 +0530

    update 1 file and create 1 file

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import upload from "../../utils/upload";
+import upload from "../../utils/upload";
 import "./Register.scss";
 import newRequest from "../../utils/newRequest";
 import { useNavigate } from "react-router-dom";
