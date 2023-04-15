e61d5fb59e73dfa904ed2a8da865f74b2d82d8d3
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-15 16:02:04 +0530

    build(deps): update package-lock.json, package.json and Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,17 +4,30 @@ import "./Login.scss";
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Sign in</h1>
         <label htmlFor="">Username</label>
-        <input name="username" type="text" placeholder="johndoe" />
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+          onChange={(e) => setUsername(e.target.value)}
+        />
 
         <label htmlFor="">Password</label>
-        <input name="password" type="password" />
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
       </form>
     </div>
